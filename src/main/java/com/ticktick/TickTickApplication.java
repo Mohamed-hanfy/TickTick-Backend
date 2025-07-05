d1aca22269c68c9fa6d75a13d7e3f14b6e96238a
Ebrahem Mordy <hemaasr25@gmail.com>
2025-07-06 01:17:58 +0300

    initialize Spring Boot application with Maven setup

---
+++ src/main/java/com/ticktick/TickTickApplication.java
@@ -0,0 +1,13 @@
+package com.ticktick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickTickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickTickApplication.class, args);
+	}
+
+}
